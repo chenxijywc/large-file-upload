37862514788773e868634d6df2317fa06e2b470f
dafu0719 <64018073@qq.com>
2023-01-04 23:22:20 +0800

    no message

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 var bodyParser = require('body-parser');
+const os = require('os')
+
+const port = 3000
 
 //创建wab 服务器
 const app = express()
@@ -10,14 +13,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 //监听post请求
-app.post('/updata', (req,res)=>{
+app.post('./updata', (req,res)=>{
     //通过post的方式传递过来的参数,通过req.query是无法获取的,因为post的请求参数在请求体里
     //要拿到通过post方式传递过来的参数,就要使用第三方包:body-parser:json对象,multer:formData
     //body-parser:req.body multer:req.file,req.body
     console.log(req,'req')
+    res.send({
+        result:1,
+        msg:'请求成功'
+    })
 })
 
-//启动服务器, 设置端口号，启动成功回调
-app.listen(3000,()=>{
-    console.log('启动成功了')
-})
+// 获取ip地址
+function getIPAdress() {
+    var interfaces = os.networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+  }
+
+  app.listen(port, () => {
+    console.log(`${getIPAdress()}:${port}`,'启动成功')
+  })
