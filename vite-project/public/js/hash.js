22f8d7e5e4b1b3c654dcb9b5226b6e855423b55a
dafu0719 <64018073@qq.com>
2023-01-17 12:37:57 +0800

    解决md5bug

--- vite-project/public/js/hash.js
+++ vite-project/public/js/hash.js
@@ -1,35 +1,34 @@
 self.importScripts("spark-md5.min.js")
 
 self.onmessage = e =>{
-  var file = e.data.file
+  let file = e.data.file
   //切片
-  var chunkSize = 1024*1024*3  // 每个切片的大小定位3m
-  var chunknum = Math.ceil(file.size / chunkSize)  // 切片数量
-  console.log(chunknum,'chunknum')
-  var sparkMD5 = new SparkMD5.ArrayBuffer()
+  let chunkSize = 1024*1024*3  // 每个切片的大小定位3m
+  let chunknum = Math.ceil(file.size / chunkSize)  // 切片数量
+  let sparkMD5 = new SparkMD5.ArrayBuffer()
   //利用文件首尾分片的md5拼接作为整个文件md5
-  for (var i = 0; i < chunknum; i++) {
-    var reader = new FileReader()
-    var park = file.slice(i * chunkSize, (i + 1) * chunkSize)
-    //md5追加计算第一片和最后一片切片
+  for (let i = 0; i < chunknum; i++) {
+    let park = file.slice(i * chunkSize, (i + 1) * chunkSize)
     if (i === 0 && chunknum !== 1) {
-      reader.readAsArrayBuffer(park)
-      reader.onload = (e) => {
-        sparkMD5.append(e.target.result)
-      }
-    }
-    if (i === chunknum - 1) {
-      reader.readAsArrayBuffer(park)
-      reader.onload = (e) => {
-        sparkMD5.append(e.target.result)
-        var md = sparkMD5.end()
+      loadNext(park)
+    }else if (i === chunknum - 1) {
+      loadNext(park,()=>{
+        let md = sparkMD5.end()
         self.postMessage({name:"succee",data:md})
         self.close()
-      }
+      })
     }
   }
-  reader.onerror = (err) => {
-    self.postMessage({name:"error",data:err})
-    self.close()
+  function loadNext(park,cd){
+    let reader = new FileReader()
+    reader.readAsArrayBuffer(park)
+    reader.onload = (e) => {
+      sparkMD5.append(e.target.result)
+      cd ? cd() : ''
+    }
+    reader.onerror = (err) => {
+      self.postMessage({name:"error",data:err})
+      self.close()
+    }
   }
 }
